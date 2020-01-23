b70faabf0bd263fa2a2b499755c327fba38c18bd
Kai Page <kaibug@gmail.com>
2020-01-23 17:48:31 -0600

    Slow mode

--- src/main/java/frc/robot/subsystems/drive/JoystickDriveCommand.java
+++ src/main/java/frc/robot/subsystems/drive/JoystickDriveCommand.java
@@ -5,6 +5,11 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class JoystickDriveCommand extends CommandBase {
     private final DriveSubsystem drive;
 
+    private boolean slowMode, toggleLastFrame;
+
+    private static final double SLOW_SPEED = 0.5;
+    private static final double FAST_SPEED = 0.9;
+
     public JoystickDriveCommand(DriveSubsystem drive) {
         addRequirements(drive);
         this.drive = drive;
@@ -12,6 +17,13 @@ public class JoystickDriveCommand extends CommandBase {
 
     @Override
     public void execute() {
-        drive.arcadeDrive(drive.container.pilot.getRawAxis(1), -drive.container.pilot.getRawAxis(4));
+        if (!toggleLastFrame && drive.container.pilot.getRawButton(2)) {
+            slowMode = !slowMode;
+            toggleLastFrame = true;
+        } else if (!drive.container.pilot.getRawButton(2)) {
+            toggleLastFrame = false;
+        }
+        var factor = slowMode ? SLOW_SPEED : FAST_SPEED;
+        drive.arcadeDrive(factor * drive.container.pilot.getRawAxis(1), -factor * drive.container.pilot.getRawAxis(4));
     }
 }
