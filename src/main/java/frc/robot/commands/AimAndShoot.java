5671f1ea9031418be1358f3391ffcaf7ebe72e9a
Kai Page <kaibug@gmail.com>
2020-02-21 17:34:34 -0600

    Changed AimAndShoot to extend ParallelDeadlineGroup

--- src/main/java/frc/robot/commands/AimAndShoot.java
+++ src/main/java/frc/robot/commands/AimAndShoot.java
@@ -1,23 +1,16 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.HoldSubsystems;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.drive.VisionAlignCommand;
 import frc.robot.subsystems.shooter.RunShooter;
 
-public class AimAndShoot {
-    private AimAndShoot() {
-    }
-
-    public static Command make(RobotContainer container) {
-        var drive = container.drive;
-        var intake = container.intake;
-        var accumulator = container.accumulator;
-        var shooter = container.shooter;
-
-        return CommandGroupBase.deadline(CommandGroupBase.race(new RunShooter(accumulator, shooter, 130000, 1000),
-                new VisionAlignCommand(drive)), new HoldSubsystems(intake));
+public class AimAndShoot extends ParallelDeadlineGroup {
+    public AimAndShoot(RobotContainer container) {
+        super(CommandGroupBase.race(new RunShooter(container.accumulator, container.shooter, 130000, 1000),
+                new VisionAlignCommand(container.drive)));
+        addCommands(new HoldSubsystems(container.intake));
     }
 }
