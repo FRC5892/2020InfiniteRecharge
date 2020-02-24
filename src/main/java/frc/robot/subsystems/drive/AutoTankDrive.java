28b9a22be45e9ae72fa7e01e9f1044c6c9a176cf
Kai Page <kaibug@gmail.com>
2020-02-24 17:25:24 -0600

    Autonomous driving

---
+++ src/main/java/frc/robot/subsystems/drive/AutoTankDrive.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class AutoTankDrive extends CommandBase {
+    private final DriveSubsystem drive;
+    private final double left;
+    private final double right;
+    private final double encoderTarget;
+
+    public AutoTankDrive(DriveSubsystem drive, double left, double right, double encoderTarget) {
+        this.drive = drive;
+        this.left = left;
+        this.right = right;
+        this.encoderTarget = encoderTarget;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void initialize() {
+        drive.resetEncoders();
+    }
+
+    @Override
+    public void execute() {
+        drive.tankDrive(left, right);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drive.getLeftEncoder() > encoderTarget || drive.getRightEncoder() > encoderTarget;
+    }
+    @Override
+    public void end(boolean interrupted) {
+        drive.tankDrive(0, 0);
+    }
+
+}
--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.drive;
 
+import com.revrobotics.CANEncoder;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.MotorSpecs;
@@ -9,12 +11,16 @@ import frc.robot.RobotMap;
 public class DriveSubsystem extends SubsystemBase {
     public final RobotContainer container;
     private final DifferentialDrive drive;
+    private final CANEncoder leftEncoder, rightEncoder;
 
     public DriveSubsystem(RobotMap map, RobotContainer container) {
         this.container = container;
-        drive = new DifferentialDrive(MotorSpecs.makeSpeedControllers(map.leftDrive),
-                MotorSpecs.makeSpeedControllers(map.rightDrive));
+        var left = MotorSpecs.makeSparkMaxes(map.leftDrive);
+        var right = MotorSpecs.makeSparkMaxes(map.rightDrive);
+        drive = new DifferentialDrive(left, right);
         addChild("Drive Train", drive);
+        leftEncoder = left.getEncoder();
+        rightEncoder = right.getEncoder();
 
         setDefaultCommand(new JoystickDriveCommand(this));
     }
@@ -23,9 +29,27 @@ public class DriveSubsystem extends SubsystemBase {
         arcadeDrive(xSpeed, zRotation, 1);
     }
 
-    // See https://www.desmos.com/calculator/snimyobj8h for why scaleFactor gets its own argument
+    // See https://www.desmos.com/calculator/snimyobj8h for why scaleFactor gets its
+    // own argument
     public void arcadeDrive(double xSpeed, double zRotation, double scaleFactor) {
         drive.arcadeDrive(Math.copySign(xSpeed * xSpeed, xSpeed) * scaleFactor,
                 Math.copySign(zRotation * zRotation, zRotation) * scaleFactor, false);
+    }
+
+    public void tankDrive(double left, double right) {
+        drive.tankDrive(left, right, false);
+    }
+
+    public double getLeftEncoder() {
+        return leftEncoder.getPosition();
+    }
+
+    public double getRightEncoder() {
+        return rightEncoder.getPosition();
+    }
+
+    public void resetEncoders() {
+        leftEncoder.setPosition(0);
+        rightEncoder.setPosition(0);
     }
 }
